7f1ed8d3110703983386edfcefaf6ddb0768c8d7
Raanch <39557164+Raanch@users.noreply.github.com>
2021-05-22 13:49:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,7 +66,7 @@ function setup(){
  bg=createSprite(windowWidth/2,windowHeight/2,windowWidth, windowHeight+100)
  bg.addImage(b5)
  bg.velocityY=12;
- bg.scale=10
+ bg.scale=5
 
  man=createSprite(windowWidth/2,windowHeight/2+150)
  man.addAnimation("background",manImg)
